7e3747713fae5c4c8cb35f21d16c5b6554163a3c
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2021-07-23 11:21:44 -0300

    Formatation update

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import 'reflect-metadata';
-import express from 'express';
+import 'reflect-metadata'
+import express from 'express'
 
-import './database';
+import './database'
 
-const app = express();
+const app = express()
 
 // http:localhost:3000
-app.listen(3000, () => console.log('Sever is Running'));
+app.listen(3000, () => console.log('Sever is Running'))
