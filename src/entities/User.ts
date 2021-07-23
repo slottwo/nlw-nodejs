7d08011a0a1846e89f8a006b150b2327e3962469
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2021-07-23 11:18:57 -0300

    User Entity seted (TS)

---
+++ src/entities/User.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm'
+
+import { v4 as uuid } from 'uuid'
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  admin: boolean
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+  /**
+   *
+   */
+  constructor() {
+    // super();
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+}
