b54bb1bc2d63ce195ed5ed1dd3003acb93d68d00
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2021-07-24 17:34:02 -0300

    Fix entity error

--- src/repositories/UsersRepositories.ts
+++ src/repositories/UsersRepositories.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm'
 import { User } from '../entities/User'
 
-@EntityRepository()
+@EntityRepository(User)
 export class UsersRepositories extends Repository<User> {}
