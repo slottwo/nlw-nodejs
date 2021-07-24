9a7511dbb6e8a366914c6e61ec9492c3d1245d22
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2021-07-24 17:40:12 -0300

    User post route and controller

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,32 @@
 import 'reflect-metadata'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+
+import { router } from './routes'
 
 import './database'
 
 const app = express()
 
-// http:localhost:3000
+// Setting the app to use json
+app.use(express.json())
+
+// Router
+app.use(router)
+
+// Middleware (Error Handling)
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({ error: err.message })
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error'
+    })
+  }
+)
+
+// baseURL: http://localhost:3000
 app.listen(3000, () => console.log('Sever is Running'))
