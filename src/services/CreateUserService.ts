2c0ce0a7afdb6c909b9b320fa2a4ce4d031817d6
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2021-07-26 20:17:13 -0300

    Fix: admin param not optional

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -10,7 +10,7 @@ interface IUserRequest {
 }
 
 export class CreateUserService {
-  async execute({ name, email, admin, password }: IUserRequest) {
+  async execute({ name, email, admin = false, password }: IUserRequest) {
     const userRepository = getCustomRepository(UsersRepositories)
 
     // trowing erros { email empty, duplicate email }
