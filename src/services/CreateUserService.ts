9dda74376b12e3f3f38635c4c2e4d7e060615ee8
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2021-07-26 10:50:39 -0300

    Dependencies update (bcryptjs)

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs'
 import { getCustomRepository } from 'typeorm'
 import { UsersRepositories } from '../repositories/UsersRepositories'
 
@@ -5,10 +6,11 @@ interface IUserRequest {
   name: string
   email: string
   admin?: boolean
+  password: string
 }
 
 export class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin, password }: IUserRequest) {
     const userRepository = getCustomRepository(UsersRepositories)
 
     // trowing erros { email empty, duplicate email }
@@ -24,8 +26,16 @@ export class CreateUserService {
       throw new Error('User already exists')
     }
 
+    // encrypting password
+    const passwordHash = await hash(password, 8)
+
     // creating user
-    const user = userRepository.create({ name, email, admin })
+    const user = userRepository.create({
+      name,
+      email,
+      admin,
+      password: passwordHash
+    })
 
     // saving user
     await userRepository.save(user)
